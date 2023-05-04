0b30dbfe744fab2e72c2886f234d48fc34e79a59
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2023-05-04 15:48:30 +0530

    init

---
+++ src/main/java/com/example/leetcode_clone/LeetcodeCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.leetcode_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeCloneApplication.class, args);
+	}
+
+}
