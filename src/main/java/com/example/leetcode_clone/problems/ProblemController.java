d5fb9d7444196faba90de09e0ae75b2701fa16fb
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2023-05-12 12:44:50 +0530

    api added problemset/<list> and problems

--- src/main/java/com/example/leetcode_clone/problems/ProblemController.java
+++ src/main/java/com/example/leetcode_clone/problems/ProblemController.java
@@ -20,7 +20,7 @@ import com.example.leetcode_clone.problems.dto.ProblemDetailDTO;
 import com.example.leetcode_clone.problems.dto.ProblemListDTO;
 
 @RestController
-@RequestMapping("/api/problems")
+@RequestMapping("/api")
 public class ProblemController {
     private ProblemService problemService;
 
@@ -28,7 +28,7 @@ public class ProblemController {
         this.problemService = problemService;
     }
 
-    @GetMapping("/")
+    @GetMapping("/problemset/all")
     ResponseEntity<List<ProblemListDTO>> getProblemList(
         @RequestParam(value="page", defaultValue = "0") String pageString,
         @RequestParam(value="size", defaultValue = "2") String sizeString
@@ -40,25 +40,25 @@ public class ProblemController {
         return ResponseEntity.ok(problemListDtos);
     }
 
-    @PostMapping("/")
+    @PostMapping("/problems/")
     public ResponseEntity<ProblemDetailDTO> createProblem(@RequestBody CreateProblemDTO createProblemDTO){
         var problem = this.problemService.createProblem(createProblemDTO);
         return ResponseEntity.created(URI.create(problem.getResourceURL())).body(problem);
     }
 
-    @GetMapping("/{slug}")
+    @GetMapping("/problems/{slug}")
     public ResponseEntity<ProblemDetailDTO> getProblemBySlug(@PathVariable String slug){
         var problem = this.problemService.getProblem(slug);
         return ResponseEntity.ok(problem);
     }
 
-    @PostMapping("/{slug}")
+    @PostMapping("/problems/{slug}")
     public ResponseEntity<ProblemDetailDTO> updateProblemBySlug(@PathVariable String slug, @RequestBody CreateProblemDTO createProblemDTO){
         var problem = this.problemService.updateProblem(slug, createProblemDTO);
         return ResponseEntity.accepted().body(problem);
     }
 
-    @DeleteMapping("/{slug}")
+    @DeleteMapping("/problems/{slug}")
     ResponseEntity<String> deleteProblem(@PathVariable String slug){
         try {
             this.problemService.deleteProblem(slug);
