b3261e9891274071b064490874bc26583b1021d7
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2023-06-19 11:53:18 +0530

    working

--- src/main/java/com/example/leetcode_clone/codeupdatelistener/CodeUpdateConsumer.java
+++ src/main/java/com/example/leetcode_clone/codeupdatelistener/CodeUpdateConsumer.java
@@ -3,14 +3,21 @@ package com.example.leetcode_clone.codeupdatelistener;
 import com.example.shared.kafka.KafkaConstant;
 import com.example.shared.kafka.dto.RunCodeUpdateDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CodeUpdateConsumer {
+    @Autowired
+    SimpMessagingTemplate template;
 
     @KafkaListener(groupId = KafkaConstant.GROUP_ID_CODE_UPDATE_CONSUMER, topics = KafkaConstant.CODE_EXECUTION_UPDATE_TOPIC_NAME, containerFactory = KafkaConstant.KAFKA_CODE_UPDATE_CONSUMER_CONTAINER_FACTORY)
     public void receivedCodeUpdate(RunCodeUpdateDTO updateDTO) throws JsonProcessingException {
         System.out.println("Code Execution Update for: " + updateDTO.getUuid() + " of type: " + updateDTO.getUpdateType().toString());
+        
+        template.convertAndSend("/submittion_run/"+updateDTO.getUuid(), updateDTO);
     }
 }
