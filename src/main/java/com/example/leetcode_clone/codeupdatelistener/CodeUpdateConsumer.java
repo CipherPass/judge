be8bf0f2bcb057993dad83f4e86522794da442d0
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2023-06-20 12:36:43 +0530

    update changed to cumulative status
    Signed-off-by: Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>

--- src/main/java/com/example/leetcode_clone/codeupdatelistener/CodeUpdateConsumer.java
+++ src/main/java/com/example/leetcode_clone/codeupdatelistener/CodeUpdateConsumer.java
@@ -16,7 +16,7 @@ public class CodeUpdateConsumer {
 
     @KafkaListener(groupId = KafkaConstant.GROUP_ID_CODE_UPDATE_CONSUMER, topics = KafkaConstant.CODE_EXECUTION_UPDATE_TOPIC_NAME, containerFactory = KafkaConstant.KAFKA_CODE_UPDATE_CONSUMER_CONTAINER_FACTORY)
     public void receivedCodeUpdate(RunCodeUpdateDTO updateDTO) throws JsonProcessingException {
-        System.out.println("Code Execution Update for: " + updateDTO.getUuid() + " of type: " + updateDTO.getUpdateType().toString());
+        System.out.println("Code Execution Update for: " + updateDTO.getUuid() + " of judgement: " + updateDTO.getJudgement());
         
         template.convertAndSend("/submittion_run/"+updateDTO.getUuid(), updateDTO);
     }
